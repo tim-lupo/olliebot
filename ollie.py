b51fff4eebbfcbeb9286897edd17dc7c36211c4e
Tim Lupo <socialmedia@timlupo.com>
2016-05-11 08:07:53 -0400

    Initial Commit

---
+++ ollie.py
@@ -0,0 +1,27 @@
+import twitter
+from twitter import *
+
+t = Twitter(
+	auth=OAuth(
+	    consumer_key=''
+        consumer_secret=''
+        token=''
+        token_secret=''
+	)
+)
+
+auth = twitter.OAuth(
+    consumer_key=''
+    consumer_secret=''
+    token=''
+    token_secret=''
+)
+
+stream = twitter.stream.TwitterStream(auth=auth, domain='userstream.twitter.com')
+
+for msg in stream.user():
+    if 'direct_message' in msg:
+    	text = msg['direct_message']['text']
+    	print "Message received..."
+    	t.statuses.update(status=text)
+        print "Tweeted: %s" % text
