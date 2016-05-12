996c65a01cd845bf8e68cbcb98dc2abe9ba2f353
Tim Lupo <socialmedia@timlupo.com>
2016-05-12 08:20:34 -0400

    Bug Fixes
    Fixed bot crashing when spammed

--- ollie.py
+++ ollie.py
@@ -3,18 +3,18 @@ from twitter import *
 
 t = Twitter(
 	auth=OAuth(
-	    consumer_key='Hie6B2gTK7DxZ7NkV4GZPsWMd',
-        consumer_secret='gdvQgSMH7weKm7mtWLTOggVMn3wFe5WaMgbRFAQFmIN3sJ5s2V',
-        token='4049670291-kEklQ68NeyfjV6TLEeKl0HgO0FcGMBKUVUdrDyF',
-        token_secret='AH2T7EhChEU8gp6EP4gYZVbVATYyi3IwZs7bkUyofPp7X'
+	    consumer_key='',
+        consumer_secret='',
+        token='',
+        token_secret=''
 	)
 )
 
 auth = twitter.OAuth(
-    consumer_key='Hie6B2gTK7DxZ7NkV4GZPsWMd',
-    consumer_secret='gdvQgSMH7weKm7mtWLTOggVMn3wFe5WaMgbRFAQFmIN3sJ5s2V',
-    token='4049670291-kEklQ68NeyfjV6TLEeKl0HgO0FcGMBKUVUdrDyF',
-    token_secret='AH2T7EhChEU8gp6EP4gYZVbVATYyi3IwZs7bkUyofPp7X'
+    consumer_key='',
+    consumer_secret='',
+    token='',
+    token_secret=''
 )
 
 stream = twitter.stream.TwitterStream(auth=auth, domain='userstream.twitter.com')
@@ -23,5 +23,8 @@ for msg in stream.user():
     if 'direct_message' in msg:
     	text = msg['direct_message']['text']
     	print "Message received..."
-    	t.statuses.update(status=text)
-        print "Tweeted: %s" % text
+    	try:
+    		t.statuses.update(status=text)
+        	print "Tweeted: %s" % text
+        except:
+        	print "Error tweeting: %s" % text
